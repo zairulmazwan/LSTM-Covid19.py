509153dbad4c61fd732bbb29fca7bfcf37b836f2
Zairul Mazwan <zairulmazwan@gmail.com>
2021-04-02 12:56:54 +0100

    a dataset deleted

--- TestCode - createDataset.py
+++ TestCode - createDataset.py
@@ -12,7 +12,8 @@ print("Length ",len(dataframe))
 def create_dataset(dataset, look_back=1):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back-1):
-		a = dataset[i:(i+look_back), 0]
+		# a = dataset[i:(i+look_back), 0]
+		a = dataset[i, 0]
 		dataX.append(a)
 		dataY.append(dataset[i + look_back, 0])
 	return numpy.array(dataX), numpy.array(dataY)
